0b587e645fa392dfff84ffbaf6fbc35ea0fcc050
kapr001 <kapr0001@stud.kea.dk>
2025-09-28 21:00:16 +0200

    ændringer 2

--- mig.js
+++ mig.js
@@ -112,3 +112,26 @@ function initSlideshow2(root) {
     dots[index - 1]?.classList.add("active");
   }
 }
+
+// BACK TO TOP KNAP MED W3 SCHOOLS
+// Knappen hentes
+let mybutton = document.getElementById("myBtn");
+
+// Hvorår knap hentes
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Skroller til top v klik
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
